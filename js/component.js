74898f26f40ae70d6c3ed8720b84edd7815eab26
Erick Merchant <erickmerchant@gmail.com>
2018-02-18 17:58:51 -0800

    Upgrade standard

--- js/component.js
+++ js/component.js
@@ -10,10 +10,10 @@ module.exports = function ({state, dispatch, next}) {
       <input autofocus onkeyup=${escapeMaybe} oninput=${setTerm} value="${state.term}" name="input" placeholder="Press enter to add." class="full-width fit-width padding-2 bold border-radius border-gray background-white placeholder-gray" />
     </form>
     <main class="auto overflow-auto">${
-      state.tasks.length
-      ? html`<div class="margin-0">
+  state.tasks.length
+    ? html`<div class="margin-0">
           ${state.tasks.map(function (task) {
-            return html`<div class="grid columns-3 items-center padding-2">
+    return html`<div class="grid columns-3 items-center padding-2">
               <div class="${task.isActive ? 'border-left-large-blue' : 'border-left-large-gray'} padding-2"><strong>${task.title}</strong></div>
               <div>
                 ${icon('clock')}
@@ -21,22 +21,22 @@ module.exports = function ({state, dispatch, next}) {
               </div>
               <div class="align-right">
                 <button class="background-white border-none margin-1" type="button" onclick=${() => dispatch('toggle', task.uuid)}>${
-                  task.isActive
-                  ? icon('pause')
-                  : icon('play')
-                }</button>
+  task.isActive
+    ? icon('pause')
+    : icon('play')
+}</button>
                 <button class="background-white border-none margin-1" type="button" onclick=${() => dispatch('copy', task.uuid)}>${icon('copy')}</button>
                 <button class="background-white border-none margin-1" type="button" onclick=${() => dispatch('remove', task.uuid)}>${icon('delete')}</button>
               </div>
             </div>`
-          })}
+  })}
         </div>`
-      : html`<p class="padding-1 align-center">${
-        state.term === ''
+    : html`<p class="padding-1 align-center">${
+      state.term === ''
         ? `You're not tracking anything yet.`
         : `No results.`
-      }</p>`
-    }</main>
+    }</p>`
+}</main>
     <footer class="background-light-gray padding-2 bold">
       <span class="dark-gray padding-vertical-1">${icon('clock')} <span class="padding-right-1">${format(state.tasks, state.now)}</span></span>
     </footer>
