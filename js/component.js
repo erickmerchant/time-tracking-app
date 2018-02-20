0794db0a4f18841c9c4b5432493f9d88e15642bd
Erick Merchant <erickmerchant@gmail.com>
2018-02-19 22:19:29 -0800

    Do not nest html tagged templates

--- js/component.js
+++ js/component.js
@@ -9,38 +9,34 @@ module.exports = function ({state, dispatch, next}) {
     <form onsubmit=${add} class="padding-3 full-width fit-width background-light-gray">
       <input autofocus onkeyup=${escapeMaybe} oninput=${setTerm} value="${state.term}" name="input" placeholder="Press enter to add." class="full-width fit-width padding-2 bold border-radius border-gray background-white placeholder-gray" />
     </form>
-    <main class="auto overflow-auto">${
-  state.tasks.length
-    ? html`<div class="margin-0">
-          ${state.tasks.map(function (task) {
-    return html`<div class="grid columns-3 items-center padding-2">
-              <div class="${task.isActive ? 'border-left-large-blue' : 'border-left-large-gray'} padding-2"><strong>${task.title}</strong></div>
-              <div>
-                ${icon('clock')}
-                ${format(task, state.now)}
-              </div>
-              <div class="align-right">
-                <button class="background-white border-none margin-1" type="button" onclick=${() => dispatch('toggle', task.uuid)}>${
-  task.isActive
-    ? icon('pause')
-    : icon('play')
-}</button>
-                <button class="background-white border-none margin-1" type="button" onclick=${() => dispatch('copy', task.uuid)}>${icon('copy')}</button>
-                <button class="background-white border-none margin-1" type="button" onclick=${() => dispatch('remove', task.uuid)}>${icon('delete')}</button>
-              </div>
-            </div>`
-  })}
-        </div>`
-    : html`<p class="padding-1 align-center">${
-      state.term === ''
-        ? `You're not tracking anything yet.`
-        : `No results.`
-    }</p>`
-}</main>
+    <main class="auto overflow-auto">${main()}</main>
     <footer class="background-light-gray padding-2 bold">
       <span class="dark-gray padding-vertical-1">${icon('clock')} <span class="padding-right-1">${format(state.tasks, state.now)}</span></span>
     </footer>
   </body>`
+
+  function main () {
+    return state.tasks.length
+      ? html`<div class="margin-0">
+          ${state.tasks.map(item)}
+        </div>`
+      : html`<p class="padding-1 align-center">${state.term === '' ? `You're not tracking anything yet.` : `No results.`}</p>`
+  }
+
+  function item (task) {
+    return html`<div class="grid columns-3 items-center padding-2">
+      <div class="${task.isActive ? 'border-left-large-blue' : 'border-left-large-gray'} padding-2"><strong>${task.title}</strong></div>
+      <div>
+        ${icon('clock')}
+        ${format(task, state.now)}
+      </div>
+      <div class="align-right">
+        <button class="background-white border-none margin-1" type="button" onclick=${() => dispatch('toggle', task.uuid)}>${icon(task.isActive ? 'pause' : 'play')}</button>
+        <button class="background-white border-none margin-1" type="button" onclick=${() => dispatch('copy', task.uuid)}>${icon('copy')}</button>
+        <button class="background-white border-none margin-1" type="button" onclick=${() => dispatch('remove', task.uuid)}>${icon('delete')}</button>
+      </div>
+    </div>`
+  }
 
   function setTerm (e) {
     dispatch('term', this.value)
