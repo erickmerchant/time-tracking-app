f2867fb207c31e02f39fb20e96a14666a899de7c
Erick Merchant <erickmerchant@gmail.com>
2018-08-11 23:06:07 -0700

    Rename component to body

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const framework = require('@erickmerchant/framework')
 const diff = require('nanomorph')
 const store = require('./store')
-const component = require('./component')
+const component = require('./body')
 const target = document.body
 
 framework({target, diff, component, store})
